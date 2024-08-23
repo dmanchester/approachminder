05ce975d5ee8bc357050b4bf4e558588eed93487
Daniel Manchester <dpmanchester@gmail.com>
2024-08-23 07:08:08 -0400

    TimeOrderedData add'l refinements

--- analysis/src/test/scala/com/dmanchester/approachminder/TimeOrderedDataSpec.scala
+++ analysis/src/test/scala/com/dmanchester/approachminder/TimeOrderedDataSpec.scala
@@ -13,6 +13,12 @@ class TimeOrderedDataSpec extends Specification {
     val time15Loser1 = TimeWithSomeText(15, "loser 1")
     val time15Loser2 = TimeWithSomeText(15, "loser 2")
 
+    "demonstrate at compile time it's retaining the subtypes of Seq and HasTime it receives" in {
+      val times: IndexedSeq[TimeWithSomeText] = IndexedSeq(time10, time21)
+      val timeOrderedData: TimeOrderedData[TimeWithSomeText, IndexedSeq[TimeWithSomeText]] = TimeOrderedData.create(times)
+      true must beTrue  // nothing to do at runtime
+    }
+
     "time-order the data" in {
       val times = Seq(time20, time10, time21)
       val timeOrderedData = TimeOrderedData.create(times)
@@ -22,7 +28,7 @@ class TimeOrderedDataSpec extends Specification {
     "resolve time-conflicting elements, picking the positionally last element with a given time as the winner" in {
       val times = Seq(time21, time15Loser1, time10, time15Loser2, time15Winner, time20)
       val timeOrderedData = TimeOrderedData.create(times)
-      timeOrderedData.seq mustEqual IndexedSeq(time10, time15Winner, time20, time21)
+      timeOrderedData.seq mustEqual Seq(time10, time15Winner, time20, time21)
     }
   }
 }
