a616167c8a41eaf5b7dee2b2d8e7d1e04e44b2ba
Daniel Manchester <dpmanchester@gmail.com>
2025-03-22 09:13:14 -0400

    Document residual use of absolute URLs, workaround

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -10,6 +10,33 @@ import { viteExternalsPlugin } from 'vite-plugin-externals'
 //
 // See also: https://vitejs.dev/config/.
 
+// FIXME The configuration below of "base: './'" notwithstanding, some references to "libs"-based assets are still using
+// absolute URLs, as opposed to relative ones (i.e., "/libs/..." as opposed to "./libs/...").
+//
+// When a production build is deployed to a non-root directory on dmanchester.com--for example, to
+// https://www.dmanchester.com/am/2022-12-d/--this use of absolute URLs leads to 404 errors on various assets, including:
+//
+//     "InfoBoxDescription.css"
+//     various "tycho*.jpg" files
+//     "IAU2006_XYS_17.json"
+//
+// This culminates in a UI error:
+//
+//     "An error occurred while rendering. Rendering has stopped."
+//
+// The 404s are due to the assets being requested from:
+//
+//     https://www.dmanchester.com/libs/...
+//
+// As opposed to:
+//
+//     https://www.dmanchester.com/am/2022-12-d/libs/...
+//
+// WORKAROUND (from Bash on dmanchester.com):
+//
+//     cd /var/www/html
+//     ln -s am/2022-12-d/libs/
+//
 const copyCesium = items =>
     viteStaticCopy({
       targets: [
