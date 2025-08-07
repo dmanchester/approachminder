aba5045a810f886ff0dac568cb086f203fc82b44
Daniel Manchester <dpmanchester@gmail.com>
2025-08-06 22:07:04 -0400

    Add roundDownToNearestMultiple()

--- analysis/src/main/scala/com/dmanchester/approachminder/utils/MathUtils.scala
+++ analysis/src/main/scala/com/dmanchester/approachminder/utils/MathUtils.scala
@@ -11,4 +11,20 @@ object MathUtils {
    * @return The interpolated value.
    */
   def interpolateScalar(a: Double, b: Double, percentage: Double): Double = a + percentage * (b - a)
+
+  /**
+   * Round a value down to the nearest multiple of a step size.
+   *
+   * If the value is a multiple of the step size, returns it unchanged.
+   *
+   * This function's behavior is undefined for negative values and negative step sizes.
+   *
+   * @param value The value.
+   * @param stepSize The step size.
+   * @return The result.
+   */
+  def roundDownToNearestMultiple(value: BigDecimal, stepSize: BigDecimal): BigDecimal = {
+    val (divisionIntegralValue, _) = value /% stepSize
+    stepSize * divisionIntegralValue
+  }
 }
--- analysis/src/test/scala/com/dmanchester/approachminder/utils/MathUtilsSpec.scala
+++ analysis/src/test/scala/com/dmanchester/approachminder/utils/MathUtilsSpec.scala
@@ -1,18 +1,27 @@
 package com.dmanchester.approachminder.utils
 
 import com.dmanchester.approachminder.SharedResources.significantFigures
-import com.dmanchester.approachminder.utils.MathUtils.interpolateScalar
+import com.dmanchester.approachminder.utils.MathUtils.{interpolateScalar, roundDownToNearestMultiple}
 import org.specs2.mutable.*
 
 class MathUtilsSpec extends Specification {
 
   "interpolateScalar" should {
-
     "interpolate" in {
       interpolateScalar(-0.5, 9.5, 0.95) must beCloseTo(9.0 within significantFigures)
       // The distance from -0.5 to 9.5 is 10.0.
       // 95% of 10.0 is 9.5.
       // -0.5 + 9.5 = 9.0.
+    }
+  }
+
+  "roundDownToNearestMultiple" should {
+    "handle a typical case" in {
+      roundDownToNearestMultiple(BigDecimal("7.7"), BigDecimal("0.5")) mustEqual BigDecimal("7.5")
+    }
+
+    "handle the case that the value is a multiple of the step size" in {
+      roundDownToNearestMultiple(BigDecimal("4.4"), BigDecimal("1.1")) mustEqual BigDecimal("4.4")
     }
   }
 }
