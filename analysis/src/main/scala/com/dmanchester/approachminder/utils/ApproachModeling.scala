bcdd369b10dcaf42296e6636da87b75250cb1e04
Daniel Manchester <dpmanchester@gmail.com>
2025-08-31 22:41:17 -0400

    Move meanTrajectory
    Also, delete ExtractionAndEstimation (now empty), along with its tests
    file (also empty).

--- analysis/src/main/scala/com/dmanchester/approachminder/utils/ApproachModeling.scala
+++ analysis/src/main/scala/com/dmanchester/approachminder/utils/ApproachModeling.scala
@@ -31,4 +31,15 @@ object ApproachModeling {
       MeanAngleAndAltitude(meanAngle, angleStdDevInDegrees, meanAltitudeInMeters, altitudeStdDevInMeters, positions.size)
     }
   }
+
+  def meanTrajectory(trajectories: Iterable[Map[BigDecimal, AngleAndAltitude]]): Map[BigDecimal, MeanAngleAndAltitude] = {
+
+    // Collect the set of distances for which at least one trajectory has a position.
+    val distancesInMeters = trajectories.flatMap(_.keys).toSet
+
+    distancesInMeters.flatMap { thisDistance =>
+      val positionsAtThisDistance = trajectories.flatMap(_.get(thisDistance))
+      ApproachModeling.calculateMeanAngleAndAltitude(positionsAtThisDistance).map(thisDistance -> _)
+    }.toMap
+  }
 }
--- analysis/src/test/scala/com/dmanchester/approachminder/utils/ApproachModelingSpec.scala
+++ analysis/src/test/scala/com/dmanchester/approachminder/utils/ApproachModelingSpec.scala
@@ -29,8 +29,54 @@ class ApproachModelingSpec extends Specification {
       meanAngleAndAltitude.positionsCount mustEqual 3
     }
 
-    "throw on insufficient positions" in {
+    "return None on insufficient positions" in {
       ApproachModeling.calculateMeanAngleAndAltitude(onePosition) must beNone
+    }
+  }
+
+  "meanTrajectory" should {
+
+    "calculate mean positions at all distances where at least two positions are available" in {
+
+      // The three trajectories for this test are laid out as follows (format: angle, altitude),
+      // giving rise to two mean positions (format: mean angle (angle variance), mean altitude
+      // (altitude variance)):
+      //
+      // Distance    Traj. A     Traj. B     Traj. C            Mean Position        Positions
+      // --------   ---------   ---------   ---------       ----------------------   ---------
+      //   1.0      1.0, 10.0       X           X
+      //   1.5      2.0, 12.0   3.0, 12.0       X      ==>  2.5 (0.5), 12.0 (0.0)        2
+      //   2.0      3.0, 14.0   6.0, 16.0   9.0, 24.0       6.0 (9.0), 18.0 (28.0)       3
+      //   2.5         X        9.0, 20.0       X
+
+      val bd_1_0 = BigDecimal("1.0")  // "bd" = "BigDecimal"
+      val bd_1_5 = BigDecimal("1.5")
+      val bd_2_0 = BigDecimal("2.0")
+      val bd_2_5 = BigDecimal("2.5")
+
+      // FIXME Put in real altitude values; and check them at bottom
+
+      val trajectories = Seq(
+        Map(bd_1_0 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(1.0), 10.0), bd_1_5 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(2.0), 12.0), bd_2_0 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(3.0), 14.0)),
+        Map(bd_1_5 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(3.0), 12.0), bd_2_0 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(6.0), 16.0), bd_2_5 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(9.0), 20.0)),
+        Map(bd_2_0 -> AngleAndAltitude(PolarAngle.fromCompassDegrees(9.0), 24.0))
+      )
+
+      val meanTrajectory = ApproachModeling.meanTrajectory(trajectories)
+
+      meanTrajectory.size mustEqual 2
+
+      meanTrajectory(bd_1_5).angle.asCompassDegrees must beCloseTo(2.5 within significantFigures)
+      meanTrajectory(bd_1_5).angleStdDevInDegrees must beCloseTo(0.707107 within significantFigures)
+      meanTrajectory(bd_1_5).altitudeInMeters must beCloseTo(12.0 within significantFigures)
+      meanTrajectory(bd_1_5).altitudeStdDevInMeters must beCloseTo(0.0 within significantFigures)
+      meanTrajectory(bd_1_5).positionsCount mustEqual 2
+
+      meanTrajectory(bd_2_0).angle.asCompassDegrees must beCloseTo(6.0 within significantFigures)
+      meanTrajectory(bd_2_0).angleStdDevInDegrees must beCloseTo(3.0 within significantFigures)
+      meanTrajectory(bd_2_0).altitudeInMeters must beCloseTo(18.0 within significantFigures)
+      meanTrajectory(bd_2_0).altitudeStdDevInMeters must beCloseTo(5.291503 within significantFigures)
+      meanTrajectory(bd_2_0).positionsCount mustEqual 3
     }
   }
 }
