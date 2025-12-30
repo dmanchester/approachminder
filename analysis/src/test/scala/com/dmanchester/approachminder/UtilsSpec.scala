954205c22d234bf43414ec4a1025c641f98b539f
Daniel Manchester <dpmanchester@gmail.com>
2025-12-30 17:16:49 -0500

    Remove unneeded "mostCommon..." methods

--- analysis/src/test/scala/com/dmanchester/approachminder/UtilsSpec.scala
+++ analysis/src/test/scala/com/dmanchester/approachminder/UtilsSpec.scala
@@ -1,20 +1,7 @@
 package com.dmanchester.approachminder
 
-import com.dmanchester.approachminder.Utils.mostCommonString
 import org.specs2.mutable.*
 
 class UtilsSpec extends Specification {
 
-  "mostCommonString" should {
-
-    "determine the most-common string, picking the alphabetically first one among equally common ones" in {
-      val strings = Seq("Cherry", "Apple", "Banana", "Apple", "Cherry")
-      mostCommonString(strings) mustEqual(Some("Apple"))
-    }
-
-    "return None if there are no strings" in {
-      val strings = Seq()
-      mostCommonString(strings) must beNone
-    }
-  }
 }
