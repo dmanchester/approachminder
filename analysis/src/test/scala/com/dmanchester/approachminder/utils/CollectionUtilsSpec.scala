ef4a920ed10ca30c9fa4acb42e4ffe6614ed0877
Daniel Manchester <dpmanchester@gmail.com>
2025-12-30 15:42:35 -0500

    Add a test to CollectionUtilsSpec

--- analysis/src/test/scala/com/dmanchester/approachminder/utils/CollectionUtilsSpec.scala
+++ analysis/src/test/scala/com/dmanchester/approachminder/utils/CollectionUtilsSpec.scala
@@ -7,19 +7,24 @@ class CollectionUtilsSpec extends Specification {
 
   "mostCommonNonBlankCategoryInNonEmptyCollection" should {
 
+    "determine the most-common non-blank category" in {
+      val categories = Seq(NoInfoAtAll, Small, NoADSBEmitterCategoryInfo, Small, Small, Light, Light, Large, NoInfoAtAll, NoADSBEmitterCategoryInfo, NoInfoAtAll, NoADSBEmitterCategoryInfo)
+      CollectionUtils.mostCommonNonBlankCategoryInNonEmptyCollection(categories) must beSome(beEqualTo(Small))
+    }
+
     "determine the most-common non-blank category, picking the alphabetically first one among equally common ones" in {
       val categories = Seq(NoInfoAtAll, NoADSBEmitterCategoryInfo, Small, Small, Light, Light, Large, NoInfoAtAll, NoADSBEmitterCategoryInfo, NoInfoAtAll, NoADSBEmitterCategoryInfo)
       CollectionUtils.mostCommonNonBlankCategoryInNonEmptyCollection(categories) must beSome(beEqualTo(Light))
     }
 
-    "return `None` if all categories are blank" in {
+    "return None if all categories are blank" in {
       val categories = Seq(NoInfoAtAll, NoADSBEmitterCategoryInfo)
       CollectionUtils.mostCommonNonBlankCategoryInNonEmptyCollection(categories) must beNone
     }
 
     "throw if there are no categories" in {
-      val categories = Seq()
-      CollectionUtils.mostCommonNonBlankCategoryInNonEmptyCollection(categories) must throwA[UnsupportedOperationException]
+      val categories = Seq.empty
+      CollectionUtils.mostCommonNonBlankCategoryInNonEmptyCollection(categories) must throwAn[UnsupportedOperationException]
     }
   }
 }
