26470d43e6d5dcb0f83a9de463afbb13ccad0841
Daniel Manchester <dpmanchester@gmail.com>
2026-04-10 22:52:40 -0400

    Use 2 spaces when nesting HTML levels, not 4

--- visualization/src/AircraftTable.svelte
+++ visualization/src/AircraftTable.svelte
@@ -23,55 +23,55 @@
 </script>
 
 <table id={showApproachSegments ? "table-with-segments" : "table-without-segments"}>
-    <thead>
+  <thead>
+  <tr>
+    <th>Callsign</th>
+    <th>Latitude</th>
+    <th>Longitude</th>
+    <th>Altitude</th>
+    {#if showApproachSegments}
+      <th class="td-emphasis">Airport</th>
+      <th class="td-emphasis">Runway</th>
+      <th class="td-emphasis">Dist. to Threshold</th>
+      <th class="td-emphasis">Vertical Deviation</th>
+      <th class="td-emphasis">Horiz. Deviation</th>
+      <th class="td-emphasis">Standard Devs.</th>
+    {/if}
+    <th>Velocity</th>
+    <th>True Track</th>
+    <th>Vertical Rate</th>
+    <th>Report Age</th>
+  </tr>
+  </thead>
+  <tbody>
+  {#each posWrappersSorted as wrapper (wrapper.position.trajectory.icao24)}
     <tr>
-        <th>Callsign</th>
-        <th>Latitude</th>
-        <th>Longitude</th>
-        <th>Altitude</th>
-        {#if showApproachSegments}
-            <th class="td-emphasis">Airport</th>
-            <th class="td-emphasis">Runway</th>
-            <th class="td-emphasis">Dist. to Threshold</th>
-            <th class="td-emphasis">Vertical Deviation</th>
-            <th class="td-emphasis">Horiz. Deviation</th>
-            <th class="td-emphasis">Standard Devs.</th>
-        {/if}
-        <th>Velocity</th>
-        <th>True Track</th>
-        <th>Vertical Rate</th>
-        <th>Report Age</th>
+      <td class="align-center">
+        <button onclick={wrapper.trackEntityFunc}>
+          {wrapper.position.trajectory.callsign}
+        </button>
+      </td>
+      <!-- FIXME Handle nulls better (for example, currently, squawk shows as "null" when not present. -->
+      <!-- TODO Can we establish intermediate vars to make the following dot lookups shorter? -->
+      <!-- TODO For fields with a decimal component, show trailing zeros (e.g., "1.0")  -->
+      <td>{wrapper.position.latitude}°</td>
+      <td>{wrapper.position.longitude}°</td>
+      <td>{numberFormat.format(wrapper.position.altitude)} m</td>  <!-- TODO Need to add in some factor to address "height above ellipsoid" vs. "height above geoid", get to a plausible height above MSL -->
+      {#if showApproachSegments}  <!-- TODO Better option than using "?" on each line below? -->
+        <td class="td-emphasis">{wrapper.position.approachSegment?.airport}</td>
+        <td class="td-emphasis">{wrapper.position.approachSegment?.threshold}</td>
+        <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.thresholdDistanceMeters ?? -1)} m</td>  <!-- TODO Here and below, "?? -1" definitely not optimal. Better option? -->
+        <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.verticalDevMeters ?? -1)} m</td>
+        <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.horizontalDevMeters ?? -1)} m</td>
+        <td class="td-emphasis">{wrapper.position.approachSegment?.normalizedEuclideanDistance}</td>
+      {/if}
+      <td>{wrapper.position.velocity} m/s</td>
+      <td>{wrapper.position.trueTrack}°</td>
+      <td>{wrapper.position.verticalRate} m/s</td>
+      <td>{wrapper.ageSecs} s.</td>
     </tr>
-    </thead>
-    <tbody>
-    {#each posWrappersSorted as wrapper (wrapper.position.trajectory.icao24)}
-        <tr>
-            <td class="align-center">
-                <button onclick={wrapper.trackEntityFunc}>
-                    {wrapper.position.trajectory.callsign}
-                </button>
-            </td>
-            <!-- FIXME Handle nulls better (for example, currently, squawk shows as "null" when not present. -->
-            <!-- TODO Can we establish intermediate vars to make the following dot lookups shorter? -->
-            <!-- TODO For fields with a decimal component, show trailing zeros (e.g., "1.0")  -->
-            <td>{wrapper.position.latitude}°</td>
-            <td>{wrapper.position.longitude}°</td>
-            <td>{numberFormat.format(wrapper.position.altitude)} m</td>  <!-- TODO Need to add in some factor to address "height above ellipsoid" vs. "height above geoid", get to a plausible height above MSL -->
-            {#if showApproachSegments}  <!-- TODO Better option than using "?" on each line below? -->
-                <td class="td-emphasis">{wrapper.position.approachSegment?.airport}</td>
-                <td class="td-emphasis">{wrapper.position.approachSegment?.threshold}</td>
-                <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.thresholdDistanceMeters ?? -1)} m</td>  <!-- TODO Here and below, "?? -1" definitely not optimal. Better option? -->
-                <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.verticalDevMeters ?? -1)} m</td>
-                <td class="td-emphasis">{numberFormat.format(wrapper.position.approachSegment?.horizontalDevMeters ?? -1)} m</td>
-                <td class="td-emphasis">{wrapper.position.approachSegment?.normalizedEuclideanDistance}</td>
-            {/if}
-            <td>{wrapper.position.velocity} m/s</td>
-            <td>{wrapper.position.trueTrack}°</td>
-            <td>{wrapper.position.verticalRate} m/s</td>
-            <td>{wrapper.ageSecs} s.</td>
-        </tr>
-    {/each}
-    </tbody>
+  {/each}
+  </tbody>
 </table>
 
 <style>
